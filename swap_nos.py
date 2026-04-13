3e27e6ba03414d6a2d886acfcd64a03e4ff18c63
Agnesmoph <morphineagnes@gmail.com>
2026-04-13 07:11:23 +0300

    my first commit

---
+++ swap_nos.py
@@ -0,0 +1,17 @@
+#a=input("enter value of a: ")
+#b=input("enter value of b: ")
+#temp=a
+#a=b
+#b=temp
+#print("a"+ a)
+#print("b"+ b)
+
+#x=5
+#y="Emprex"
+#print(x)
+#print(y)
+
+x=15
+y="Emprex"
+print(type(x))
+print(type(y))
