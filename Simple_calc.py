7ff1388ced3edc38d4b1afd8efee330606e94f64
Agnesmoph <morphineagnes@gmail.com>
2026-04-14 23:41:32 +0300

    Updated project

---
+++ Simple_calc.py
@@ -0,0 +1,15 @@
+#num1=16
+#num2=21
+#sum=num1+num2
+#print(sum)
+#area of a redctangle
+#length=15
+#width=17
+#area=length*width
+#print (area)
+
+#temperature converter
+#cel=17
+#far=(cel*9/5)+32
+#print (far)
+
